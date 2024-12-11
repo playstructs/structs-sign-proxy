1186bba8fcacc2994e8d7bdb24f5e78c306c344a
abstrct <abstrct@slow.ninja>
2024-12-11 14:13:15 -0500

    New codebase

---
+++ src/structs-sign-proxy.js
@@ -0,0 +1,31 @@
+
+
+//Parameters
+
+// Type [guild join, address register]
+// Mnemonic
+
+// Player ID
+
+// Guild ID
+// Nonce
+
+
+// Initiate wallet
+
+// Get account
+
+// Build message
+
+// Sha256 msg
+
+// sign message
+
+// encode results
+
+// encode pub key
+
+
+// output as json?
+
+
